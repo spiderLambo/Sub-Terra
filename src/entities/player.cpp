e0b3d0300d3506542d4b3e6a46695096b2556b89
Spider Lambo <spiderLambo.dev@outlook.com>
2026-07-23 05:24:16 +0200

    [+]>Getters

--- src/entities/player.cpp
+++ src/entities/player.cpp
@@ -1,3 +1,18 @@
 #include "entities/player.h"
 
-void hello() { std::cout << "hello world" << std::endl; }
+Player::Player(int rang, std::string couleur) : rang(rang), couleur(couleur) {
+  this->pv = 3;
+  this->conscient = true;
+  this->cache = false;
+  this->actions = 3;
+}
+
+Player::~Player() {}
+
+// Getters
+int Player::getPv() const { return this->pv; }
+int Player::getRang() const { return this->rang; }
+std::string Player::getCouleur() const { return this->couleur; }
+bool Player::estCache() const { return this->cache; }
+bool Player::estConscient() const { return this->conscient; }
+int Player::getActions() const { return this->actions; }
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
 #include "main.h"
 
-int main() { return 0; }
+int main() {
+  Player p(1, "#FFFFFF");
+  std::cout << "Pv : " << p.getPv() << std::endl
+            << "Rang : " << p.getRang() << std::endl
+            << "Couleur : " << p.getCouleur() << std::endl
+            << "Cache : " << p.estCache() << std::endl
+            << "Conscient : " << p.estConscient() << std::endl
+            << "Actions : " << p.getActions() << std::endl;
+  return 0;
+}
